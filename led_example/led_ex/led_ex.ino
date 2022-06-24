31c1bb7e7f924fc9aecfc7734d804beb33135c94
Pedro Villa <villapedro2@gmail.com>
2022-06-23 21:23:57 -0700

    Expanded LED example to accept commands in serial port to turn on/off, and blink LED

--- led_example/led_ex/led_ex.ino
+++ led_example/led_ex/led_ex.ino
@@ -1,9 +1,20 @@
+/*
+ * Expanded code to include serial inputs to control LED 
+ * on -> turns on LED
+ * off -> turns off LED
+ * blink -> turns on/off LED ten times
+ */
+
 
 int status;
+int i;
+String command;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(13, OUTPUT); // set built in led as ouput
+  Serial.begin(9600);
+  Serial.println("Enter command:");
 
 }
 
@@ -13,8 +24,37 @@ void loop() {
    * analogWrite(pin, duty cycle)
    * duty cycle maps value 0-255 to 0-100%
    */
-  analogWrite(13, 127); 
-  status = analogRead(13);
-  Serial.print(status);
-  Serial.print("\n");
+   if (Serial.available()){
+    command = Serial.readStringUntil('\n');
+    if (command.equals("blink")){
+      blinkLED();
+     }
+     else if (command.equals("on")){
+      on();
+     }
+     else if (command.equals("off")){
+      off();
+     }
+     else{
+      Serial.println("Invalid command");
+     } 
+    }
+}
+
+void blinkLED(){
+  for (i=0; i<=10; i++){
+    digitalWrite(13, HIGH);
+    delay(100);
+    digitalWrite(13, LOW);
+    delay(100); // Second delay is needed otherwise timing issue occurs and breaks function
+    i;
+    }
+}
+
+void on(){
+  digitalWrite(13, HIGH);
+}
+
+void off(){
+  digitalWrite(13, LOW);
 }
